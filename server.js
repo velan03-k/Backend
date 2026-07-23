ed55dd230a5f1e6dc0969513d458ebc82cfedff6
velan03-k <velavank2000@gmail.com>
2026-07-23 10:19:16 +0530

    Error resolved

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ mongoose
 
 // Routes
 app.use("/api/users", require("./Routers/UserRouter"));
-app.use("/api/users/login", require("./Routers/UserRouter"));
 app.use("/api/doctors", require("./Routers/DoctorRouter"));
 app.use("/api/departments", require("./Routers/Department"));
 app.use("/api/applications", require("./Routers/ApplicationRouter"));
