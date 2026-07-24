eaaa4c4719e9e96cf9578c0500e1877f360245b9
velan03-k <velavank2000@gmail.com>
2026-07-24 10:12:38 +0530

    Application update operation

--- Routers/ApplicationRouter.js
+++ Routers/ApplicationRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {createApplication} = require("../Controllers/aplicationController")
+const {createApplication, updateApplication} = require("../Controllers/aplicationController")
 const {getApplication} = require("../Controllers/aplicationController")
 
 router.post('/', createApplication);
 router.get('/', getApplication);
-router.put("/:id", updateApplication);
+router.put("/:id", updateApplicationpdateApplication);
 
 module.exports = router;
